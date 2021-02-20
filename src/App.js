b40bd4aad2635247f83b8c4e32953b1e7f7a6679
kamal.deol2013@gmail.com <kamal.deol2013@gmail.com>
2021-02-20 14:56:47 +0530

    some testing for BOSS

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Some testing ... lets c
+        Kamalpreet Singh
       </header>
     </div>
   );
